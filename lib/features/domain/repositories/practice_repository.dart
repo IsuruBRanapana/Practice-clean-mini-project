5a17237c5a17e35804ab22fa3c8accd517899e01
isuru_r <isuru_r@epiclanka.net>
2020-07-21 20:21:33 -0700

    Updated

--- lib/features/domain/repositories/practice_repository.dart
+++ lib/features/domain/repositories/practice_repository.dart
@@ -3,6 +3,7 @@ import 'package:practice/core/errors/failures.dart';
 import 'package:practice/features/domain/entities/problem.dart';
 import 'package:practice/features/domain/entities/request/login_request_email_password.dart';
 import 'package:practice/features/domain/entities/response/login_response.dart';
+import 'package:practice/features/domain/entities/user.dart';
 
 abstract class PracticeRepository {
   Future<Either<Failure, Problem>> getAllProblems();
@@ -12,6 +13,8 @@ abstract class PracticeRepository {
   Future<Either<Failure, Problem>> getCategorizeProblems(String category);
 
   Future<Either<Failure, Problem>> getDistrictProblems(String district);
+
+  Future<Either<Failure, User>> registerUser(String district);
 
   Future<Either<Failure, LoginResponse>> getUser(
       LoginRequestEmailPassword params);
